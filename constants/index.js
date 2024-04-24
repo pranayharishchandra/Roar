01ec573f2e22a6de6767a5cbd0207aba9623ef56
pranay <pranayharishchandra@gmail.com>
2024-04-24 17:18:16 +0530

    renamed

--- constants/index.js
+++ constants/index.js
@@ -32,7 +32,7 @@ export const sidebarLinks = [
 ];
 
 export const profileTabs = [
-  { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
+  { value: "threads", label: "Posts", icon: "/assets/reply.svg" },
   { value: "replies", label: "Replies", icon: "/assets/members.svg" },
   { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" },
 ];
